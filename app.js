036d4ea660bffbba1f4179eb839045e69f8f602b
Sajay Antony <sajaya@microsoft.com>
2016-04-14 13:55:35 -0700

    Added birthmessage of clientconfig details.

--- app.js
+++ app.js
@@ -29,7 +29,13 @@ var settings = {
         broker: "172.30.168.182",
         clientid: os.hostname(),
         topic: os.hostname(),
-        clientips: GetClientIPs()
+        clientconfig: {
+            "hostname": os.hostname(),
+            "arch": os.arch(),
+            "ostype": os.type(),
+            "os": os.platform(),
+            "ips": GetClientIPs()
+        }
     }
 };
 
@@ -57,7 +63,7 @@ function GetClientIPs() {
     var ifaces = os.networkInterfaces();
     var ips = [];
     Object.keys(ifaces).forEach(function (ifname) {
-        var alias = 0;        
+        var alias = 0;
         ifaces[ifname].forEach(function (iface) {
             if ('IPv4' !== iface.family || iface.internal !== false) {
                 // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
@@ -66,7 +72,7 @@ function GetClientIPs() {
             ips.push(iface.address);
             if (alias >= 1) {
                 // this single interface has multiple ipv4 addresses
-                console.log(ifname + ':' + alias, iface.address);                
+                console.log(ifname + ':' + alias, iface.address);
             } else {
                 // this interface has only one ipv4 adress
                 console.log(ifname, iface.address);

514315238cc6225037cc5dd40edcc405656fdf07
Sajay Antony <sajaya@microsoft.com>
2016-04-19 16:19:38 -0700

    Fixed pid and hostname casing.

--- client/cmdport.js
+++ client/cmdport.js
@@ -21,21 +21,29 @@ function cli() {
     commist.register('send', require('./node_modules/mqtt/bin/pub'));
     commist.register('subscribe', require('./node_modules/mqtt/bin/sub'));
     commist.register('clean', require('./clean'));
-    
-    commist.register('version', function() {    
+
+    commist.register('version', function () {
         console.log('MQTT.js version:', require('./node_modules/mqtt/package.json').version);
     });
 
     commist.register('help', helpMe.toStdout);
-    var options = 
+    var options =
         [
-            "--hostname", broker.host, 
+            "--hostname", broker.host,
             "--username", broker.username,
             "--password", broker.password,
             "-q", "1"
         ];
-
+    
     var args = process.argv.slice(2).concat(options);
+        
+    // Canonicalize the hostname.
+    for (var i = 0; i <= args.length; i++) {
+        if (args[i] == '-t') {
+            args[i + 1] = args[i + 1].toLowerCase();
+        }
+    }
+    
 
     if (null !== commist.parse(args)) {
         console.log('No such command:', '\n');

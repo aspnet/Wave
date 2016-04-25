20a3a5923bb38c04aef1684274382d6132e4cb78
Sajay Antony <sajaya@microsoft.com>
2016-04-25 13:33:46 -0700

    Windows setup script

---
+++ scripts/Install.ps1
@@ -0,0 +1,34 @@
+﻿param(
+     [Parameter(Mandatory = $true, ValueFromPipeline = $true)] 
+     [string]$url,
+     [ValidateScript({[System.IO.Path]::IsPathRooted($_)})]
+     [string]$target,
+     [string]$broker,
+     [string]$username,
+     [string]$password
+)
+
+if(test-path $target)
+{
+    Write-Error "$target already exists"
+    exit 1;
+}
+
+mkdir -p $target
+$zipfilePath = Join-Path $target "_temp_download.zip"
+
+"Downloading [$url]`n"
+$client = new-object System.Net.WebClient
+$client.DownloadFile( $url, $zipfilePath )
+
+"Unzipping to [$target]`n"
+#Load the assembly
+[System.Reflection.Assembly]::LoadWithPartialName("System.IO.Compression.FileSystem") | Out-Null
+#Unzip the file
+[System.IO.Compression.ZipFile]::ExtractToDirectory($zipfilePath, $target);
+
+pushd $target
+node setup.js $broker $username $password
+
+npm install -g forver 
+forever start app.js

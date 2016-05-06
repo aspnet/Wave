6f85e5e563fd3e371ef7d557a8fd68aa08af56f4
Sajay <sajaya@microsoft.com>
2016-05-06 00:53:34 -0700

    Add a clean command to remove stale nodes from the broker. Make status and output non-retained and QOS 0.

--- client/clean.js
+++ client/clean.js
@@ -2,6 +2,7 @@
 'use strict';
 
 var mqtt = require('mqtt')
+    , colors = require('colors')
     , path = require('path')
     , fs = require('fs')
     , concat = require('concat-stream')
@@ -12,86 +13,26 @@ var mqtt = require('mqtt')
 
 function drain(args) {
     args = minimist(args, {
-        string: ['hostname', 'username', 'password', 'key', 'cert', 'ca'],
-        integer: ['port', 'qos', 'keepAlive'],
-        boolean: ['stdin', 'help', 'clean', 'insecure'],
+        boolean: ['delete-offline'],
         alias: {
-            port: 'p',
-            hostname: ['h', 'host'],
             topic: 't',
-            qos: 'q',
-            clean: 'c',
-            keepalive: 'k',
-            clientId: ['i', 'id'],
-            username: 'u',
-            password: 'P',
-            protocol: ['C', 'l'],
-            verbose: 'v',
-            help: '-H',
-            ca: 'cafile'
+            qos: 'q'
         },
         default: {
-            host: 'localhost',
-            qos: 0,
-            retain: false,
-            clean: true,
-            keepAlive: 30 // 30 sec
+            topic: 'client/#',
+            qos: 0
         }
     })
 
-    if (args.help) {
-        return helpMe.toStdout('subscribe');
+    if(!args['delete-offline']){
+        console.log(colors.yellow("Specify --delete-offline to delete the stale configs"));
     }
-
-    args.topic = args.topic || args._.shift();
-
-    if (!args.topic) {
-        console.error('missing topic\n')
-        return helpMe.toStdout('subscribe');
-    }
-
-    if (args.key) {
-        args.key = fs.readFileSync(args.key);
-    }
-
-    if (args.cert) {
-        args.cert = fs.readFileSync(args.cert);
-    }
-
-    if (args.ca) {
-        args.ca = fs.readFileSync(args.ca);
-    }
-
-    if (args.key && args.cert && !args.protocol) {
-        args.protocol = 'mqtts';
-    }
-
-    if (args.insecure) {
-        args.rejectUnauthorized = false;
-    }
-
-    if (args.port) {
-        if (typeof args.port !== 'number') {
-            console.warn('# Port: number expected, \'%s\' was given.', typeof args.port);
-            return;
-        }
-    }
-
-    if (args['will-topic']) {
-        args.will = {};
-        args.will.topic = args['will-topic'];
-        args.will.payload = args['will-message'];
-        args.will.qos = args['will-qos'];
-        args.will.retain = args['will-retain'];
-    }
-
-    args.keepAlive = args['keep-alive'];
 
     var client = mqtt.connect(args);
 
-    client.on('connect', function() {
-        client.subscribe(args.topic, { qos: args.qos }, function(err, result) {
-            result.forEach(function(sub) {
+    client.on('connect', function () {
+        client.subscribe(args.topic, { qos: args.qos }, function (err, result) {
+            result.forEach(function (sub) {
                 if (sub.qos > 2) {
                     console.error('subscription negated to', sub.topic, 'with code', sub.qos);
                     process.exit(1);
@@ -102,51 +43,67 @@ function drain(args) {
 
     // Wait for 3 seconds
     var waitTime = 3000;
-    var slidingwindow = {
-        delta: waitTime,
-        previous: new Date()
+    var lastMsg = {
+        timestamp: new Date()
     };
 
-    setInterval(function() {
-        if (slidingwindow.delta < waitTime) {
+    setInterval(function () {
+        var current = new Date();
+        var delta = current - lastMsg.timestamp;
+        console.log("Wating... - [%s] ", delta/1000);
+        if (delta < waitTime) {
             return true;
         }
+
         console.log("No messages during the last 3 seconds and so stopping clean. You can try running clean again to wait for more.")
         client.end();
         process.exit(0);
     }, waitTime);
 
-    client.on('message', function(topic, payload) {
-        var timestamp = new Date();
-        slidingwindow.delta = timestamp - slidingwindow.previous;
-        slidingwindow.previous = timestamp;
-
-        if (args.verbose) {
-            console.log(topic, payload.toString())
-        } else {
-            console.log(payload.toString())
-        }
+    var deleted = {};
+    client.on('message', function (topic, payload) {
+        lastMsg.timestamp = new Date();
+        TryDelete(client, topic, payload, args, deleted)
     });
 }
 
-function start(args) {
 
-    // drain all messages     
-    var cleanargs = minimist(args, {
-        alias: {
-            topic: 't',
+function TryDelete(client, topic, payload, args, deleted) {
+    var shouldDelete = true;
+    var days = -1;
+    var offline = true;
+    try {
+        payload = JSON.parse(payload);
+        if (payload.timestamp) {
+            var sourceDateTime = new Date(payload.timestamp);
+            var current = new Date();
+            days = Math.round((current - sourceDateTime) / (1000 * 60 * 60 * 24));
+            offline = payload.status != 'online';
         }
-    });
+    } catch (e) {
+    }
 
-    args = args.concat([
-        "--no-clean",
-        "-i", cleanargs.topic]);
+    if ((days == -1) || offline) {
+        days = colors.red(days)
+    }
+    else {
+        days = colors.green("online " + days);
+        topic = colors.green(topic);
+        shouldDelete = false;
+    }
 
-    drain(args);
+    console.log("%s \t%s days %s", topic, days, shouldDelete ? "--will-delete" : 'ONLINE');
+    if (args['delete-offline'] && shouldDelete) {
+        if (!deleted[topic]) {
+            deleted[topic] = payload;
+            args.retain = true;
+            client.publish(topic, '', args);
+        }
+    }
 }
 
-module.exports = start;
+module.exports = drain;
 
 if (require.main === module) {
-    start(process.argv.slice(2))
+    drain(process.argv.slice(2))
 }

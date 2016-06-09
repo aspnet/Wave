7d969d4bae615a2f9dcf18cb500b6436fbe24f6e
Sajay Antony <sajaya@microsoft.com>
2016-06-09 16:23:16 -0700

    Handle null clientid (#34)
    * Handle null clientid
    
    * Support ClientId in the docker file.

--- scripts/docker-debug.sh
+++ scripts/docker-debug.sh
@@ -1,4 +1,4 @@
 pushd `dirname $0` > /dev/null
 docker build -t wave:debug --file Dockerfile.debug . 
 docker rm -f wave
-docker run -it  --net="host" -p 8000:8000/tcp -v `readlink -f ..`:/wave --name wave wave:debug 
+docker run -it  --net="host" -p 8000:8000/tcp -v `readlink -f ..`:/wave --name wave "$@" wave:debug 
